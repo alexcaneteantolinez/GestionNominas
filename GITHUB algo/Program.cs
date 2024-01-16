5adf30ce69b1716ff514ba6c9a90e36aad5f625e
DAM1 <DAM1@DESKTOP-GPPEBEN>
2024-01-16 08:53:05 +0100

    Agregar archivos de proyecto.

---
+++ GITHUB algo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GITHUB_algo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
